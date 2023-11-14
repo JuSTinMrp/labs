01e2a706938e74f3f1f731f41d42d1994907f3e0
JuSTinMrp <r4mp3nt3st3r619@gmail.com>
2023-11-14 11:12:44 +0530

    sample_page

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const deployBtn = document.getElementById('deployBtn');
+    const stopBtn = document.getElementById('stopBtn');
+    const timerDisplay = document.getElementById('timer');
+    let deploymentTime = 0;
+    let timerInterval;
+
+    deployBtn.addEventListener('click', function () {
+        // Call a backend endpoint to deploy the instance
+        // Start a timer
+        deploymentTime = 0;
+        timerInterval = setInterval(function () {
+            deploymentTime++;
+            timerDisplay.textContent = `Deployment Time: ${deploymentTime} minutes`;
+        }, 60000);
+    });
+
+    stopBtn.addEventListener('click', function () {
+        // Call a backend endpoint to stop the instance
+        // Stop the timer
+        clearInterval(timerInterval);
+        timerDisplay.textContent = `Deployment Time: ${deploymentTime} minutes`;
+    });
+});
