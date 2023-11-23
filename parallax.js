e4179c3576fe36e1b6883ce11165a0806b73de52
JuSTinMrp <r4mp3nt3st3r619@gmail.com>
2023-11-23 19:21:44 +0530

    completed parallex

--- parallax.js
+++ parallax.js
@@ -2,8 +2,8 @@ document.addEventListener("mousemove", parallax);
 function parallax(event) {
     this.querySelectorAll(".mouse").forEach((shift) => {
         const position = shift.getAttribute("value");
-        const x = (window.innerWidth - event.pageX * position) / 360;
-        const y = (window.innerHeight - event.pageY * position) / 360;
+        const x = (window.innerWidth - event.pageX * position) / 90;
+        const y = (window.innerHeight - event.pageY * position) / 90;
 
         shift.style.transform = `translateX(${x}px) translateY(${y}px)`;
     });
